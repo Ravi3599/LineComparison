28d266d616e4ca4ad159889322cfeb43ea854f2c
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-11 14:21:36 +0530

    UPDATED:calculated length of line

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -12,6 +12,7 @@ public class LineComparisonComputation {
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
         double len = Math.sqrt((x2 - x1) * (x2 - x1)  +  (y2 - y1) * (y2 - y1));
+
         System.out.println("Length of the given Line is : " + len + " units" );
     }
 }
